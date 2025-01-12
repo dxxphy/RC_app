62f66724774c94feff08f9b8bdfe0e4c9699f703
Wenxi Xu <xuwenxi0517@gmail.com>
2025-01-12 13:31:36 +0800

    修改好了ekf，将它独立出来了

--- IMU/src/main.c
+++ IMU/src/main.c
@@ -1,4 +1,4 @@
-/*	
+/*
  * Copyright (c) 2024 ttwards <12411711@mail.sustech.edu.cn>
  *
  * SPDX-License-Identifier: Apache-2.0
@@ -12,16 +12,15 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
-#include "vofa/justfloat.c"
-#include "ekf/QuaternionEKF.c"
-#include <math.h>
+#include <ares/vofa/justfloat.c>
+#include <ares/ekf/imu_task.c>
 
 #undef PI
 #define PI 3.14159265f
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
-#define UART_NODE DT_NODELABEL(usart6)
+#define UART_NODE DT_NODELABEL(usart3)
 const struct device *uart_dev = DEVICE_DT_GET(UART_NODE);
 
 #define ACCEL_NODE DT_NODELABEL(bmi08x_accel)
@@ -30,22 +29,19 @@ const struct device *accel_dev = DEVICE_DT_GET(ACCEL_NODE);
 #define GYRO_NODE DT_NODELABEL(bmi08x_gyro)
 const struct device *gyro_dev = DEVICE_DT_GET(GYRO_NODE);
 
-struct JFData data;
+int main(void)
+{
+	struct JFData *data = jf_send_init(uart_dev, 15, 4);
 
-int main(void) {
-    jf_send_init(uart_dev, &data, 100);
+	k_sleep(K_MSEC(50));
+	IMU_Sensor_trig_init(accel_dev, gyro_dev);
 
-    // motor_control(motor1, ENABLE_MOTOR);
-    // motor_set_mode(motor1, MIT);
-    k_sleep(K_MSEC(50));
+	int t = 0;
+	while (1) {
+		IMU_QuaternionEKF_Observe(&QEKF_INS.IMU_QuaternionEKF);
+		memcpy(data->fdata, QEKF_INS.q, sizeof(QEKF_INS.q));
 
-    int t = 0;
-    while (1) {
-        data.fdata[0] = sinf(PI * t / 1000.0f);
-        data.fdata[1] = cosf(PI * t / 1000.0f);
-        data.fdata[2] = sinf(PI * t / 2000.0f);
-        data.fdata[3] = cosf(PI * t / 2000.0f);
-        k_msleep(50);
-        t += 50;
-    }
+		k_msleep(15);
+		t += 50;
+	}
 }
