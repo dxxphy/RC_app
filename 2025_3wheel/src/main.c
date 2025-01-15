21481047c190c731bb6dc1c223ac0eea2f699390
Wenxi Xu <xuwenxi0517@gmail.com>
2025-01-15 22:03:30 +0800

    对vofa函数做了一些修改，同时适应了一下插件；另外部分适配了达妙控制板

--- 2025_3wheel/src/main.c
+++ 2025_3wheel/src/main.c
@@ -9,20 +9,18 @@
 #include <zephyr/device.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/devicetree.h>
-#include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/can.h>
+#include <zephyr/debug/thread_analyzer.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
-#include <ares/vofa/justfloat.c>
-#include <ares/ekf/imu_task.c>
+#include <zephyr/drivers/motor.h>
 #include <ares/board/init.c>
-#include <sys/_intsup.h>
-#include <zephyr/debug/thread_analyzer.h>
+#include <ares/ekf/imu_task.c>
+#include <ares/vofa/justfloat.c>
 
-#undef PI
-#define PI 3.14159265f
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
-// LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
-
+/* Devicetree */
 #define UART_NODE DT_ALIAS(vofa)
 const struct device *uart_dev = DEVICE_DT_GET(UART_NODE);
 
@@ -32,37 +30,40 @@ const struct device *accel_dev = DEVICE_DT_GET(ACCEL_NODE);
 #define GYRO_NODE DT_NODELABEL(bmi08x_gyro)
 const struct device *gyro_dev = DEVICE_DT_GET(GYRO_NODE);
 
+#define MOTOR1_NODE DT_INST(0, dm_motor)
+
+const struct device *motor1 = DEVICE_DT_GET(MOTOR1_NODE);
+
 /* CAN Feedback to console*/
 K_THREAD_STACK_DEFINE(feedback_stack_area, 2048); // 定义线程栈
 void console_feedback(void *arg1, void *arg2, void *arg3)
 {
 	while (1) {
+		thread_analyzer_print(NULL);
+
+		LOG_INF("rpm: motor1: %.2f\n", (double)motor_get_speed(motor1));
 		k_msleep(500);
 	}
 }
-struct k_thread feedback_thread_data;
 
 int main(void)
 {
 	board_init();
 
-	struct JFData *data = jf_send_init(uart_dev, 25);
-
-	jf_channel_add(data, &QEKF_INS.q[0], PTR_FLOAT);
-	jf_channel_add(data, &QEKF_INS.q[1], PTR_FLOAT);
-	jf_channel_add(data, &QEKF_INS.q[2], PTR_FLOAT);
-	jf_channel_add(data, &QEKF_INS.q[3], PTR_FLOAT);
-
-	/* Start Feedback thread*/
-	k_thread_create(&feedback_thread_data, feedback_stack_area,
-			K_THREAD_STACK_SIZEOF(feedback_stack_area), console_feedback, NULL, NULL,
-			NULL, -1, 0, K_NO_WAIT);
+	struct JFData *data = jf_send_init(uart_dev, 25, 4);
 
 	k_sleep(K_MSEC(50));
 	IMU_Sensor_trig_init(accel_dev, gyro_dev);
 
+	/* Start Feedback thread*/
+	struct k_thread feedback_thread_data;
+	k_thread_create(&feedback_thread_data, feedback_stack_area,
+			K_THREAD_STACK_SIZEOF(feedback_stack_area), console_feedback,
+			(void *)motor1, NULL, NULL, 0, 0, K_MSEC(300));
+
 	int t = 0;
 	while (1) {
+		memcpy(data->fdata, QEKF_INS.q, sizeof(QEKF_INS.q));
 		k_msleep(25);
 		t += 50;
 	}
